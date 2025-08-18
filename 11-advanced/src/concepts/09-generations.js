a64a3b221c703920624dcf2649f5493944de8494
leonhardtjs <michael.amv30@gmail.com>
2025-08-17 19:50:34 -0500

    [ADD] finish section 11

--- 11-advanced/src/concepts/09-generations.js
+++ 11-advanced/src/concepts/09-generations.js
@@ -4,7 +4,39 @@
  */
 export const generationFunctionComponent = (element) => {
 
-    console.log('generationFunctionComponent')
+    // const myGenerator = myFirstGenerationFunction();
+
+    const genId = idGenerator();
+
+    const button = document.createElement('button');
+    button.innerHTML = 'Click me';
+    element.append(button);
+
+    const renderButton = () => {
+        const { value } = genId.next();
+        button.innerText = `Click ${value}`;
+    }
+
+    button.addEventListener('click', renderButton)
+
     
+}
+
+function* idGenerator() {
+    let currentId = 0;
+    while(true) {
+        yield `2023-${++currentId}`;
+    }
+}
+
+function* myFirstGenerationFunction() {
+    yield 'Primer valor';
+    yield 'Segundo valor';
+    yield 'Tercer valor';
+    yield 'Cuarto valor';
+
+
+    return 'Ya no hay valores';
+    yield 'Ya no puede hacer nada';
 }
 
