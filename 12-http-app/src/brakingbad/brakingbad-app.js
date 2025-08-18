a5b46074a782deb2f98b0c54dc923163afb17cb9
leonhardtjs <michael.amv30@gmail.com>
2025-08-18 16:30:20 -0500

    [ADD] finish section 12

---
+++ 12-http-app/src/brakingbad/brakingbad-app.js
@@ -0,0 +1,39 @@
+/**
+ * @returns {Promise<Object} quote information
+ */
+const fetchQuote = async() => {
+    const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await response.json();
+    return data[0];
+    
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const BreakingbadApp = async(element) => {
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    fetchQuote().then(renderQuote);
+
+    nextQuoteButton.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote(quote);
+    })
+
+
+}
